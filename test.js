aab7174d2f3c3b55ec54b0fd37254fbe356f77f1
Heshie Brody <4hbrody@gmail.com>
2021-06-25 01:07:08 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,11 @@
+function stringIncludes(word, letter) {
+  let matches;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      matches = true
+    }
+  }
+  return !!matches
+}
+
+console.log(stringIncludes("banana", "p"))
